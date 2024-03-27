12c302e5705c83697f1c4d70c1062514c3588b5f
Fabricio Valdez <100192392+fasit0v@users.noreply.github.com>
2024-03-27 14:39:20 -0300

    registerPage

---
+++ front/src/app/registerPage/page.tsx
@@ -0,0 +1,10 @@
+import RegisterForm from '@/components/registerForm/registerForm'
+import React from 'react'
+
+
+function RegisterPage() {
+  return (
+    <RegisterForm/>
+  )
+}
+export default RegisterPage
